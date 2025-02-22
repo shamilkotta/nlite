cec003690be66aff523b968a2cf331582a7ffea3
Shamil <shamilkotta99@gmail.com>
2025-02-23 00:16:54 +0530

    chore: pacakge rename

--- packages/lite/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { patchCssModules } from 'vite-css-modules';
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [patchCssModules(), react()]
-});
